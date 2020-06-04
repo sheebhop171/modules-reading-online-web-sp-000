d6349a84af02f404f02cc58040ff9a19c7138ad1
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-04 00:59:50 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,10 +3,12 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
+  extend MetaDancing
+
   include Dance
-
+ 
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
   end
