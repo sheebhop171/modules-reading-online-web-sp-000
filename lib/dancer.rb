9223d8734d9ee1e818f0006bdf4391c69da8a2b2
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-04 01:04:42 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,9 +4,8 @@ require_relative './fancy_dance.rb'
 
 class Dancer
 
-  extend MetaDancing
-
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
